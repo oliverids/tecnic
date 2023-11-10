428baa0bd786fffa7d218d8ecd6890cf7c37f28b
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-11-10 01:04:52 -0300

    feat(section-depoimentos): css da seção + swiperJS

--- scripts/script-swiper.js
+++ scripts/script-swiper.js
@@ -1,9 +1,19 @@
-let mainNoticiasSwiper = new Swiper(".card-slider", {
+let servicosSlider = new Swiper(".card-slider", {
   spaceBetween: 20,
   centeredSlides: false,
   slidesPerView: 'auto',
   pagination: {
     el: ".card-pagination",
+    clickable: true,
+  },
+});
+
+let depoimentosSlider = new Swiper(".depoimentos-slider", {
+  spaceBetween: 16,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  pagination: {
+    el: ".depoimento-pagination",
     clickable: true,
   },
 });
