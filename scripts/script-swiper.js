4323017729120d312a26913698637fdbe21aceee
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-11-10 00:02:19 -0300

    feat(section-servicos): css da seção + swiperJS

---
+++ scripts/script-swiper.js
@@ -0,0 +1,9 @@
+let mainNoticiasSwiper = new Swiper(".card-slider", {
+  spaceBetween: 20,
+  centeredSlides: false,
+  slidesPerView: 'auto',
+  pagination: {
+    el: ".card-pagination",
+    clickable: true,
+  },
+});
