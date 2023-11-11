e0ea6abdf1ab8ab74b6a362a6da40a1fd2196abb
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-11-11 16:35:56 -0300

    feat(section-numbers): animação de contagem dos números

--- scripts/script-main.js
+++ scripts/script-main.js
@@ -17,3 +17,45 @@ window.onclick = e => { //fecha se a sidebar se clicar fora
     header.classList.remove('ativo')
   }
 };
+
+function isInViewport(element) {
+  let rect = element.getBoundingClientRect(),
+    html = document.documentElement;
+
+  return (
+    rect.top < 500
+    // rect.left >= 0 &&
+    // rect.bottom <= (window.innerHeight || html.clientHeight) ,
+    // rect.right <= (window.innerWidth || html.clientWidth)
+  );
+}
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const numberOne = document.getElementById("number-one"),
+  numberTwo = document.getElementById("number-two"),
+  numberThree = document.getElementById("number-three");
+
+
+localStorage.setItem('animated', 'false');
+window.addEventListener('scroll', () => {
+
+  if (isInViewport(document.querySelector('.numbers')) && localStorage.getItem('animated') !== 'true') {
+    localStorage.setItem('animated', 'true');
+
+    animateValue(numberOne, 0, 130, 2000);
+    animateValue(numberTwo, 0, 150, 2000);
+    animateValue(numberThree, 0, 20, 2000);
+  }
+})
